3af762733be0fb788296af0e58b93f53ff755072
Vantuz <Shcherbina444@gmail.com>
2025-05-29 11:58:09 +0300

    Rewrite AOI request to axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
 export async function getUsers(params) {
-    return await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+    // return await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+    const {data: users} = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return users
 }
