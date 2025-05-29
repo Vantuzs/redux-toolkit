eda52a6a2e9716717002afc874cd78a3eef99d75
Vantuz <Shcherbina444@gmail.com>
2025-05-29 11:36:28 +0300

    Add createAsyncThunk basic example+ connect reducer to store

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,7 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import * as API from 'api';
 
 const SLICE_NAME = 'users';
+
+const getUsers = createAsyncThunk(`${SLICE_NAME}/getUsers`,()=>{
+    return API.getUsers();
+});
+// users/getUsers/fulfilled
+// users/getUsers/panding
+// users/getUsers/rejected
 
 const initialState = {
     users: [],
@@ -9,12 +16,30 @@ const initialState = {
     error: null
 }
 
-createSlice({
+const usersSlice = createSlice({
     name: SLICE_NAME,
     initialState,
     reducers: {
         getUsers: (state,action) => {
             // API.getUsers()
         }
+    },
+    extraReducers: {
+        [getUsers.pending]: (state,action) => {
+            state.isLoading = true;
+        },
+        [getUsers.fulfilled]: (state,action) =>{
+            state.users = action.payload;
+            state.isLoading = false;
+        },
+        [getUsers.rejected]: (state,action) =>{
+            state.error = action.payload;
+            state.isLoading = false;
+        }
     }
-})
+});
+
+// export createAsycncThunk result
+export {getUsers};
+
+export default usersSlice.reducer;
